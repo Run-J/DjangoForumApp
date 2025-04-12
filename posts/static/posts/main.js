13c62fe7542f77d91c48897c2d67266e4336e67c
Run-J <jirun040403@hotmail.com>
2025-04-12 02:50:53 -0400

    Test the ajax call with DOM is working properly

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,1 +1,19 @@
 console.log("Hello Wrold!")
+
+const helloWroldBox = document.getElementById('hello-world');
+
+
+// helloWroldBox.innerHTML = 'hello <i><b>world!<b><i>';
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWroldBox.textContent = response.text;
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
