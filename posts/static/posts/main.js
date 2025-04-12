2306ae4b60be4570daeb3b7b54788d7c97b7949f
Run-J <jirun040403@hotmail.com>
2025-04-12 04:10:14 -0400

    Getting Posts Objects with Ajax
    1: Create a new view called load_post_data_view that return the data dictionary that include all posts info in JSON format.
    2: Use ajax to get these data(posts info) from the above view.
    3: These posts info that get by ajax will be rendered to web as innerHTML format with div tag.

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log("Hello Wrold!")
 
 const helloWroldBox = document.getElementById('hello-world');
+const postsBox = document.getElementById('posts-box');
 
 
 // helloWroldBox.innerHTML = 'hello <i><b>world!<b><i>';
@@ -15,5 +16,24 @@ $.ajax({
     },
     error: function(error){
         console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data  // here is response.data, bcoz the 'data' is returned by load_post_data_view, instead of keyword.
+        console.log(data)
+        data.forEach(ele => {
+            //  here it's using ` isteaf of '
+            postsBox.innerHTML += `  
+                ${ele.title} - <b>${ele.body}</b> <br>
+            `
+        });
+    },
+    error: function(response) {
+        console.log(error)
     }
 })
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
 
+
 # Create your views here.
 
 def post_list_and_create(request):
@@ -10,3 +11,16 @@ def post_list_and_create(request):
 
 def hello_world_view(request):
     return JsonResponse({'text' : 'hello world!'})
+
+def load_post_data_view(request):
+    qs = Post.objects.all()
+    data = []
+    for obj in qs:
+        item = {
+            'id': obj.id,
+            'title': obj.title,
+            'body': obj.body,
+            'author': obj.author.user.username
+        }
+        data.append(item)
+    return JsonResponse({'data':data})
